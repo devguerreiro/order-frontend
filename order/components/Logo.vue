3c45071ae89cdb1b421522ca0386fef055514d8d
Luis <devcorujam@gmail.com>
2021-03-16 20:51:13 +0000

    ✨ Add logo component

---
+++ order/components/Logo.vue
@@ -0,0 +1,16 @@
+<template>
+  <img
+    class="object-contain"
+    src="/images/logo-mercos-white.svg"
+    alt="Logo Mercos Branca"
+    title="Logo branca da empresa Mercos"
+    :height="height"
+    :width="width"
+  />
+</template>
+
+<script>
+export default {
+  props: ['height', 'width'],
+}
+</script>
