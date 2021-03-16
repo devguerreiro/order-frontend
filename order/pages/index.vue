b26c6f0950fb3409826f48dedaa7b74bdee18737
Luis <devcorujam@gmail.com>
2021-03-16 21:08:11 +0000

    🎨 Change index page

--- order/pages/index.vue
+++ order/pages/index.vue
@@ -1,3 +1,9 @@
 <template>
-  <div></div>
+  <div class="flex flex-col justify-center items-center">
+    <h1 class="text-white text-4xl">
+      Seja Bem vindo ao
+      <strong class="border-b-4 border-mercos-green">OrderEasily</strong>
+    </h1>
+    <MercosButton class="my-8 text-2xl" label="Enviar Pedido" />
+  </div>
 </template>
