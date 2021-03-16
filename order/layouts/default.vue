92c4fcb196e34bf9447c56c3b2b8cca4f32d7a67
Luis <devcorujam@gmail.com>
2021-03-16 21:07:50 +0000

    🎨 Change default layout

--- order/layouts/default.vue
+++ order/layouts/default.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="h-full flex flex-col bg-mercos-purple">
+    <MercosNavBar />
+    <Nuxt class="flex-1" />
   </div>
 </template>
+
+<style>
+html,
+body,
+#__nuxt,
+#__layout {
+  height: 100%;
+  width: 100%;
+}
+</style>
