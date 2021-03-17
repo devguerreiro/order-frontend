1913619246d365ae22d2a6fbe1128d549c18d133
Luis <devcorujam@gmail.com>
2021-03-17 00:22:00 +0000

    ♻️ Fix h1 text align on index page

--- order/pages/index.vue
+++ order/pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col justify-center items-center">
-    <h1 class="text-white text-4xl">
+    <h1 class="text-white text-center text-4xl">
       Seja Bem vindo ao
       <strong class="border-b-4 border-mercos-green">OrderEasily</strong>
     </h1>
