a4bd954acefc3a54b983f4eb5723a46203e0e175
Luis <devcorujam@gmail.com>
2021-03-17 17:13:21 +0000

    ✨ Add navItems and mobileNavItems components

---
+++ order/components/MobileNavItems.vue
@@ -0,0 +1,26 @@
+<template>
+  <el-dropdown trigger="click">
+    <img
+      src="~/assets/icons/icon-menu-sandwich-white.svg"
+      alt="Icone Menu"
+      title="Icone Sanduiche Menu"
+      height="30"
+      width="30"
+    />
+    <el-dropdown-menu slot="dropdown">
+      <el-dropdown-item
+        v-for="(link, index) in links"
+        :key="index"
+        :icon="link.icon"
+      >
+        <nuxt-link :to="link.url">{{ link.label }}</nuxt-link>
+      </el-dropdown-item>
+    </el-dropdown-menu>
+  </el-dropdown>
+</template>
+
+<script>
+export default {
+  props: ['links'],
+}
+</script>
---
+++ order/components/NavItems.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="w-2/3 xl:w-1/2">
+    <ul class="flex justify-evenly">
+      <li v-for="(link, index) in links" :key="index">
+        <nuxt-link
+          class="border-b-2 rounded-md px-4 hover:bg-mercos-green border-mercos-green flex items-center"
+          :to="link.url"
+          >{{ link.label }}</nuxt-link
+        >
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+export default {
+  props: ['links'],
+}
+</script>
