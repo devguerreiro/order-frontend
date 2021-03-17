5e88b402978a564afe4a5863b768860e104d9731
Luis <devcorujam@gmail.com>
2021-03-17 17:06:18 +0000

    🎨 Remove conditional rendering navbar component

--- order/components/NavBar.vue
+++ order/components/NavBar.vue
@@ -1,37 +1,41 @@
 <template>
-  <div class="h-16 px-4 flex justify-between text-white md:px-8">
+  <div
+    class="h-16 px-4 flex justify-between items-center bg-mercos-purple text-white md:px-8"
+  >
     <div id="navLogo" class="w-32 flex items-center">
       <nuxt-link :to="{ name: 'index' }">
         <MercosLogo />
       </nuxt-link>
     </div>
-    <div id="mobileNavItems" v-if="isMobile" class="self-center">
+    <div id="mobileNavItems" class="md:hidden">
       <el-dropdown trigger="click">
         <img
           src="~/assets/icons/icon-menu-sandwich-white.svg"
           alt="Icone Menu"
           title="Icone Sanduiche Menu"
+          height="30"
+          width="30"
         />
         <el-dropdown-menu slot="dropdown">
           <el-dropdown-item
             v-for="(link, index) in links"
             :key="index"
             :icon="link.icon"
-            >{{ link.label }}</el-dropdown-item
           >
+            <nuxt-link :to="link.url">{{ link.label }}</nuxt-link>
+          </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
     </div>
-    <div id="navItems" v-else class="flex flex-1 justify-end items-center">
+    <div id="navItems" class="hidden md:flex flex-1 justify-end items-center">
       <nav class="w-2/3 xl:w-1/2">
         <ul class="flex justify-evenly">
           <li v-for="(link, index) in links" :key="index">
             <nuxt-link
               class="border-b-2 rounded-md px-4 hover:bg-mercos-green border-mercos-green flex items-center"
               :to="link.url"
+              >{{ link.label }}</nuxt-link
             >
-              {{ link.label }}
-            </nuxt-link>
           </li>
         </ul>
       </nav>
@@ -47,22 +51,6 @@ export default {
       { label: 'Produtos', url: { name: 'products' }, icon: 'el-icon-present' },
       { label: 'Clientes', url: '#', icon: 'el-icon-user-solid' },
     ],
-    screenSize: 0,
-    isMobile: null,
   }),
-  beforeMount() {
-    this.updateScreenSize(window.innerWidth)
-    window.addEventListener('resize', this.handleResize)
-  },
-  methods: {
-    handleResize: function (e) {
-      this.updateScreenSize(e.target.innerWidth)
-    },
-    updateScreenSize: function (newWidth) {
-      this.isMobile = this.verifyIsMobile(newWidth)
-      this.screenSize = newWidth
-    },
-    verifyIsMobile: (width) => width <= 768,
-  },
 }
 </script>
